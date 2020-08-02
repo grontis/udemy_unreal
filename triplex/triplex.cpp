5e787272b286b25d16ff4b1e356975d3764d695e
Grant K <grantkveton@msn.com>
2020-08-02 19:15:32 -0400

    progress in the simple C++ terminal number game

--- triplex/triplex.cpp
+++ triplex/triplex.cpp
@@ -2,8 +2,32 @@
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    int CodeA = 4;
+    int CodeB = 3;
+    int CodeC =  2;
 
+    int Sum = CodeA + CodeB + CodeC;
+    int Product = CodeA * CodeB * CodeC;
+
+    std::cout << "The codes add up to: " << Sum << std::endl;
+    std::cout << "The codes multiply to give: " << Product << std::endl;
+
+    int PlayerGuessA, PlayerGuessB, PlayerGuessC;
+
+    std::cout << "Enter your guess: " << std::endl;
+    std::cin >> PlayerGuessA >> PlayerGuessB >> PlayerGuessC;
+
+    int GuessSum = PlayerGuessA + PlayerGuessB + PlayerGuessC;
+    int GuessProduct = PlayerGuessA * PlayerGuessB * PlayerGuessC;
+
+    if(GuessSum == Sum && GuessProduct == Product)
+    {
+        std::cout << "You win" << std::endl;
+    }
+    else
+    {
+        std::cout << "You lose" << std::endl;
+    }
     
 
     return 0;
