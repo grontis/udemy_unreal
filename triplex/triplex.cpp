7e62224f4ba74fafa6573f8dc1019f0f27170e28
Grant K <grantkveton@msn.com>
2020-08-02 22:11:31 -0400

    Finished c++ terminal game section. Edit made to gitignore for vscode

--- triplex/triplex.cpp
+++ triplex/triplex.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <ctime>
 
-int main()
+
+
+void PrintIntroduction(int LevelDifficulty)
 {
-    int CodeA = 4;
-    int CodeB = 3;
-    int CodeC =  2;
+    std::cout << "*** Enter the correct code to access server room on level " << LevelDifficulty << " ***\n\n";
+}
+
+bool PlayGame(int LevelDifficulty)
+{
+    PrintIntroduction(LevelDifficulty);
+
+    int CodeA = rand() % LevelDifficulty + LevelDifficulty;
+    int CodeB = rand() % LevelDifficulty + LevelDifficulty;
+    int CodeC = rand() % LevelDifficulty + LevelDifficulty;
 
     int Sum = CodeA + CodeB + CodeC;
     int Product = CodeA * CodeB * CodeC;
 
     std::cout << "The codes add up to: " << Sum << std::endl;
     std::cout << "The codes multiply to give: " << Product << std::endl;
+    std::cout << std::endl;
 
     int PlayerGuessA, PlayerGuessB, PlayerGuessC;
 
@@ -22,13 +33,36 @@ int main()
 
     if(GuessSum == Sum && GuessProduct == Product)
     {
-        std::cout << "You win" << std::endl;
+        std::cout << "ACCESS GRANTED" << std::endl;
+        return true;
     }
     else
     {
-        std::cout << "You lose" << std::endl;
+        std::cout << "ACCESS DENIED" << std::endl;
+        return false;
     }
-    
+}
 
+int main()
+{
+    srand(time(NULL));
+
+    int LevelDifficulty = 1;
+    int MaxLevels = 5;
+
+    while(LevelDifficulty <= MaxLevels)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if(bLevelComplete)
+        {
+            LevelDifficulty++;
+        }
+    }
+
+    std::cout << "YOU WIN THE GAME\n";
+    
     return 0;
 }
